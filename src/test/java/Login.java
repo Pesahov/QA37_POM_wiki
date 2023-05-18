9061ac4731c286ec2102a9d8730d099bea4c2490
Shilol Pesahov <pesahov777@gmail.com>
2023-05-18 14:39:43 +0300

    loginTest

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -8,7 +8,7 @@ public class Login extends ConfigurationWiki {
 
     @Test
     public void  login1(){
-        User user = User.builder().userName("shilol@gmail.com").password("Shilol12345$").build();
+        User user = User.builder().userName("juliakliot.jk").password("Misha240613").build();
         boolean isLogged = new MainScreen(driver)
                 .clickTheFlowButton()
                 .cklickTheLoginButton()
@@ -17,4 +17,43 @@ public class Login extends ConfigurationWiki {
                 .isLogged();
         Assert.assertTrue(isLogged);
     }
+
+    @Test
+    public void login2(){
+        User user = User.builder().userName("juliakliot.jk").password("Misha240613").build();
+        String inf = new MainScreen(driver)
+                .clickTheFlowButton()
+                .cklickTheLoginButton()
+                .fillInLoginForm(user)
+                .clickTheFlowButton()
+                .getInf();
+        Assert.assertEquals(inf,"Juliakliot.jk");
+    }
+    @Test
+    public void login3(){
+        User user = User.builder().userName("juliakliot.jk").password("Misha240613").build();
+        String infOut= new MainScreen(driver)
+                .clickTheFlowButton()
+                .cklickTheLoginButton()
+                .fillInLoginForm(user)
+                .clickTheFlowButton()
+                .getInfOut();
+        Assert.assertEquals(infOut,"Log out");
+
+    }
+    @Test
+    public void login4(){
+        User user = User.builder().userName("juliakliot.jk").password("Misha240613").build();
+        String infLog = new MainScreen(driver)
+                .clickTheFlowButton()
+                .cklickTheLoginButton()
+                .fillInLoginForm(user)
+                .clickTheFlowButton()
+                .logOut()
+                .clickTheFlowButton()
+                .getIngLog();
+        Assert.assertEquals(infLog, "Log in to Wikipedia");
+
+    }
+
 }
