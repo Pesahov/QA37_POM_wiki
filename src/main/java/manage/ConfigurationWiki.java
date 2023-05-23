a8d1e21b3689ac50ae6ede74533360f563ca2d00
Shilol Pesahov <pesahov777@gmail.com>
2023-05-23 12:41:15 +0300

    logger+listener+dataprovider

--- src/main/java/manage/ConfigurationWiki.java
+++ src/main/java/manage/ConfigurationWiki.java
@@ -4,6 +4,8 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.events.EventFiringWebDriverFactory;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
@@ -12,19 +14,21 @@ import java.net.URL;
 
 public class ConfigurationWiki {
     protected  static AppiumDriver<MobileElement> driver;
+    protected Logger logger = LoggerFactory.getLogger(ConfigurationWiki.class);
 
     @BeforeSuite
     public void setUp() throws MalformedURLException {
         DesiredCapabilities capabilities= new DesiredCapabilities();
         capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "N37");
+        capabilities.setCapability("deviceName", "qa_new");
         capabilities.setCapability("platformVersion","8.1");
         capabilities.setCapability("appPackage", "org.wikipedia");
         capabilities.setCapability("appActivities", ".main.MainActivity");
 
         capabilities.setCapability("automationName", "Appium");
-        capabilities.setCapability("app","/Users/macbookpro/Downloads/wiki.apk");
+        capabilities.setCapability("app","C:/Users/julia/Documents/QA/Mobile/wiki.apk");
         driver = new AppiumDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+        driver = EventFiringWebDriverFactory.getEventFiringWebDriver(driver, new AppiumListener());
 
 
     }
